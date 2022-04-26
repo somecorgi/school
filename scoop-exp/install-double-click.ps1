d811c410513653936aa6e2142ccd4e2af76d6079
somecorgi <93149485+somecorgi@users.noreply.github.com>
2022-04-26 18:40:02 +0100

    Add files via upload

---
+++ scoop-exp/install-double-click.ps1
@@ -0,0 +1,7 @@
+﻿Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+iwr -useb get.scoop.sh -outfile 'install.ps1'
+.\install.ps1 -RunAsAdmin
+scoop install git
+scoop bucket add extras
+scoop install firefox 7zip audacity putty thunderbird
+C:\Users\WDAGUtilityAccount\scoop\apps\7zip\current\install-context.reg
